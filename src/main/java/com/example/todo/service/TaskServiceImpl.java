f41ade610e8947acbfe0a633fcdc67b186ac0162
Art Lyt <artlyt99@gmail.com>
2024-08-20 22:51:47 +0300

    Added lombok to auto-generate constructor of TaskServiceImpl

--- src/main/java/com/example/todo/service/TaskServiceImpl.java
+++ src/main/java/com/example/todo/service/TaskServiceImpl.java
@@ -4,18 +4,16 @@ import com.example.todo.model.Task;
 import com.example.todo.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
 
-    @Autowired
-    public TaskServiceImpl(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
-    }
 
     @Override
     public List<Task> getAllTasks() {
